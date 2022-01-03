440a8c747f612c9fc4d416776307dc9f07181f3c
Pranay7399 <pranay@goldsetu.co>
2022-01-03 14:54:58 +0530

    Chaned to home modules

--- Java/home_modules/home.java
+++ Java/home_modules/home.java
@@ -1,6 +1,6 @@
-package Java.login_modules;
+package Java.home_modules;
 
-public class login {
+public class home {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
